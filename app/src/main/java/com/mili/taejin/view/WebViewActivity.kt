eb7a89648a4af4ae661c8811dfcc156252940461
DESKTOP-A6SE9CV\K <ktkt00000@gmail.com>
2024-08-02 23:55:45 +0900

    fix: 필요없는 import 제거

--- app/src/main/java/com/mili/taejin/view/WebViewActivity.kt
+++ app/src/main/java/com/mili/taejin/view/WebViewActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,8 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.viewinterop.AndroidView
 import com.mili.taejin.ui.theme.MyApplicationTheme
